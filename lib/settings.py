23f34624674d6d1cef5fe8e0e9089fe3fa3b5208
annaartamon <a.artamonova2@eapteka.ru>
2022-01-18 15:45:57 +0300

    test commit

--- lib/settings.py
+++ lib/settings.py
@@ -1,7 +1,7 @@
-auth_headers = {"Authorization": "Basic dGVzdF9hcGk6MTIzNDU2",
-                   "api_key": "13695c175a5646f68d647ca1cdc59be0"}
+auth_headers = {"Authorization": "Basic 0JDRgNGC0LDQvNC+0L3QvtCy0LDQkDo=",
+                   "api_key": "708b5069da82427894c6ae06d6fcbfd2"}
 
-base_url = "http://vs-msk01-app01t/eapteka_t_artamonova2/hs/partners_api2_0/v2"
+base_url = "http://vs-msk01-app01t:1841/erp_d03_sudarikov/hs/partners_api2_0/v2"
 
 order_body = '''
 {
@@ -12,14 +12,14 @@ order_body = '''
             "phone":"+79152956789",
             "email":"kotik@mail.ru"
          },
-         "order_number": "00000780",
+         "order_number": "me_94455078",
          "external_id":"4acc35b6-50a7-11eb-9370-005053870080",
-         "date":"2021-08-31T16:02:46",
-         "deliverypoint_id":"595FA0D8-F2C9-11EA-9972-D89EF37D77FA",
+         "date":"2022-01-18T16:02:46",
+         "deliverypoint_id":"D6B71F29-C205-11EB-9E53-00505687D61D",
          "sum":1000,
          "positions":[
             {
-               "product_id":"32ce7b00-f2b5-11ea-9972-d89ef37d77fa",
+               "product_id":"a6d4aacc-344a-4f50-98a5-8fa94e201d53",
                "quantity":1,
                "price":1000,
                "nds":10
--- lib/temp.py
+++ lib/temp.py
@@ -1,13 +1,11 @@
-class BaseApiTest:
-    def test_auth(self):
-        assert 1 == 1
+import json
+from lib.assertions import Assertions
+from lib.settings import auth_headers, order_body
+from lib.my_requests import MyRequests
 
 
-class TestApiOrders(BaseApiTest):
-    def test_api_method(self):
-        assert 1 == 1
-
-
-class TestApiInvoce(BaseApiTest):
-    def test_api_method(self):
-        assert 1 == 1
+class TestOrders():
+    def test_order_positive(self):
+        response = MyRequests.post("/orders", headers=auth_headers, data=order_body.encode('utf-8'))
+        external_id = json.loads(order_body)["orders"][0]["external_id"]
+        Assertions.assert_accepted(response, external_id, f"{external_id} is not in accepted array")
